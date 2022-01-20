7d8fd6ed9de1834ddab0f692ab6d9293a98550b3
Deepak Saini <91534605+DeepakSaini02@users.noreply.github.com>
2022-01-20 20:20:50 +0530

    slider

--- src/components/Goal/TopEducator.jsx
+++ src/components/Goal/TopEducator.jsx
@@ -1,27 +1,61 @@
 import "./TopEducator.css"
 
+import React, { useRef, useState } from "react";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination"
+import "swiper/css/navigation"
+
+// import "./styles.css";
+
+
+// import Swiper core and required modules
+import SwiperCore, {
+  Pagination,Navigation
+} from 'swiper';
+
+// install Swiper modules
+SwiperCore.use([Pagination,Navigation]);
+
+
 export const TopEducator = ({ name, topTut }) => {
-    return <div>
-        <h1>Prepare with Top Educators</h1>
-        <p>Access to India's best {name} educators is just a subscription away</p>
-        <div>
+    return <div className="headDivtop">
+
+        <h3 className="topheadedu">Prepare with Top Educators</h3>
+        <p className="topheadedup"> Access to India's best {name} educators is just a subscription away</p>
+        <p className="See">See all</p>
+        <Swiper slidesPerView={2.3} spaceBetween={30} slidesPerGroup={2} loop={false} loopFillGroupWithBlank={true} pagination={{
+  "clickable": true,
+}} navigation={true} className="mySwiper">
+        <div className="tophead">
             {
                 topTut.map((el) => (
+                    <SwiperSlide>
                     <div className="sidebarflex">
                         <div>
                             <img id="sideimg" src={el.img} alt="" />
                         </div>
                         <div>
-                            <div>
+                            <div className="dataname">
                                 <h3>{el.name}</h3>
                                 <p>{el.desc}</p>
                                 <p>{el.teach}</p>
                             </div>
                         </div>
                     </div>
+            </SwiperSlide>
                 ))
             }
         </div>
+
+        
+        </Swiper>
+
+
+
 
     </div>
 }
