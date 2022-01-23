0b0704cfd99e4271e4833505f355dec223eef312
Deepak Saini <91534605+DeepakSaini02@users.noreply.github.com>
2022-01-23 14:57:54 +0530

    Update Config.jsx

--- src/components/utils/Config.jsx
+++ src/components/utils/Config.jsx
@@ -11,15 +11,6 @@ import firebase from "firebase";
 //   measurementId: "G-DPZBG1VD5N",
 // };
 const firebaseConfig = {
-<<<<<<< Updated upstream
-  apiKey: "AIzaSyCWDXXmfG2WxRMLBTcCcc5G7dM8E7wxmF4",
-  authDomain: "fir-auth-58d47.firebaseapp.com",
-  projectId: "fir-auth-58d47",
-  storageBucket: "fir-auth-58d47.appspot.com",
-  messagingSenderId: "582035892169",
-  appId: "1:582035892169:web:5c1d8561b67cfc6f39ea18",
-  measurementId: "G-DPZBG1VD5N",
-=======
   apiKey: "AIzaSyAjybY4pev4zMhbDRioGMETj_8I70h4gNQ",
   authDomain: "unacademy-clone-11a11.firebaseapp.com",
   projectId: "unacademy-clone-11a11",
@@ -27,7 +18,6 @@ const firebaseConfig = {
   messagingSenderId: "830572030637",
   appId: "1:830572030637:web:4ff4622053ed0b15927424",
   measurementId: "G-8Q27YGG4TR"
->>>>>>> Stashed changes
 };
 firebase.initializeApp(firebaseConfig);
 var auth = firebase.auth();
